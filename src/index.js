030c8ec18a92e22dd6615d457d215b3c6604140c
Geunsu Yeom <ycs47171@gmail.com>
2023-02-23 14:17:53 +0900

    second commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Accommodate from './chapter7/Accommodate';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Accommodate />
+//   </React.StrictMode>
+//   document.getElementById('root')
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Accommodate />
   </React.StrictMode>
 );
 
